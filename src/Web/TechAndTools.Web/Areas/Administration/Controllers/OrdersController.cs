a124f7a9540ec269c44833dcf1c4901c378603ae
Tihomir Vasilev <Rinzz86@yahoo.com>
2019-08-24 01:34:06 +0300

    remove duplicate css

--- src/Web/TechAndTools.Web/Areas/Administration/Controllers/OrdersController.cs
+++ src/Web/TechAndTools.Web/Areas/Administration/Controllers/OrdersController.cs
@@ -1,5 +1,5 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using TechAndTools.Services.Contracts;
 
 namespace TechAndTools.Web.Areas.Administration.Controllers
