b58206307648b0a14f40a75cecc51a80f6d0096c
Tihomir Vasilev <Rinzz86@yahoo.com>
2019-09-02 23:09:33 +0300

    clean some usings

--- src/Web/TechAndTools.Web.InputModels/Commons/InputModelsConstants.cs
+++ src/Web/TechAndTools.Web.InputModels/Commons/InputModelsConstants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
-
-namespace TechAndTools.Web.InputModels.Commons
+﻿namespace TechAndTools.Web.InputModels.Commons
 {
     public static class InputModelsConstants
     {
